415bc9e957173ebd13b903e7448822fd8ad665fc
tongji53 <tvxq-jyj.01180406@ezweb.ne.jp>
2020-10-21 16:02:25 +0900

    基本的なCRUD処理

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+EMAIL = "test@example.com"
+PASSWORD = "password"
+
+# データを全削除
+User.destroy_all
+
+# 以下，開発用の初期データ
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+user2.posts.create!(title: "2", content: "沖縄そば")
+user1.posts.create!(title: "1", content: "海ぶどう")
+user3.posts.create!(title: "2", content: "ゴーヤーチャンプルー")
+user3.posts.create!(title: "2", content: "サーターアンダギー")
+user1.posts.create!(title: "2", content: "タコライス")
+
+# ログイン時に使用するアカウント（変数への代入は不要）
+User.create!(email: EMAIL, password: PASSWORD)
+
+puts "初期データの投入に成功しました！"
